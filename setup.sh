6be53a2c0dc012cc5cb58c3b072d63237bf445b0
extremecoders-re <extremecoders@hotmail.com>
2022-11-09 16:58:37 +0530

    Package v2.1.2

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ check_env() {
 }
 
 install() {
-  wget https://github.com/rizinorg/cutter/releases/download/v2.1.0/Cutter-v2.1.0-Linux-x86_64.AppImage -O $APM_TMP_DIR/cutter.AppImage
+  wget https://github.com/rizinorg/cutter/releases/download/v2.1.2/Cutter-v2.1.2-Linux-x86_64.AppImage -O $APM_TMP_DIR/cutter.AppImage
   mv $APM_TMP_DIR/cutter.AppImage $APM_PKG_INSTALL_DIR/cutter.AppImage
   chmod +x $APM_PKG_INSTALL_DIR/cutter.AppImage
   ln -s $APM_PKG_INSTALL_DIR/cutter.AppImage $APM_PKG_BIN_DIR/cutter
